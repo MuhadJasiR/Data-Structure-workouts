ce1c381488bea7536e54e0fd5f83ba59054bd798
MuhadJasiR <alijasir051@gmail.com>
2023-05-06 21:13:52 +0530

    sample test commit

--- sorting/insertion_sort.dart
+++ sorting/insertion_sort.dart
@@ -1,17 +1,17 @@
-insertionSort(List array) {
-  for (var i = 0; i < array.length - 1; i++) {
-    int key = array[i];
+insertionSort(List arr) {
+  for (var i = 1; i < arr.length - 1; i++) {
+    int temp = arr[i];
     int j = i - 1;
-    while (j >= 0 && array[j] > key) {
-      array[j + 1] = array[j];
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
       j--;
     }
-    array[j + 1] = key;
+    arr[j + 1] = temp;
   }
 }
 
 void main(List<String> args) {
-  List array = [3, 6, 1, 4, 2, 7];
-  insertionSort(array);
-  print(array);
+  List arr = [4, 3, 1, 6, 2, 7];
+  insertionSort(arr);
+  print(arr);
 }
