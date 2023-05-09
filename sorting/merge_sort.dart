7eba7782d690c24b4c690b3a610a27428f689abd
MuhadJasiR <alijasir051@gmail.com>
2023-05-09 11:39:44 +0530

    test

--- sorting/merge_sort.dart
+++ sorting/merge_sort.dart
@@ -1,57 +1,61 @@
-void main(List<String> args) {
-  List<int> array = [4, 8, 9, 1, 3, 2, 8];
-
-  mergeSort(array);
-  print(array);
-}
-
-void mergeSort(List<int> array) {
-  int length = array.length;
+mergeSort(List arr) {
+  int length = arr.length;
   if (length <= 1) {
     return;
   }
-  int middle = array.length ~/ 2;
+
+  int middle = arr.length ~/ 2;
+  List leftArray = List.filled(middle, 0);
+  List rightArray = List.filled(arr.length - middle, 0);
+
   int i = 0, j = 0;
-  List<int> leftArray = List<int>.filled(middle, 0);
-  List<int> rightArray = List<int>.filled(array.length - middle, 0);
-  for (; i < array.length; i++) {
+
+  for (; i < arr.length; i++) {
     if (i < middle) {
-      leftArray[i] = array[i];
+      leftArray[i] = arr[i];
     } else {
-      rightArray[j] = array[i];
+      rightArray[j] = arr[i];
       j++;
     }
   }
 
   mergeSort(leftArray);
   mergeSort(rightArray);
-  merge(array, leftArray, rightArray);
+  merge(arr, leftArray, rightArray);
 }
 
-void merge(List<int> array, List<int> leftArray, List<int> rightArray) {
+void merge(List arr, List leftArray, List rightArray) {
   int i = 0, l = 0, r = 0;
-  int leftSize = array.length ~/ 2;
-  int rightSize = array.length - leftSize;
+
+  int leftSize = arr.length ~/ 2;
+  int rightSize = arr.length - leftSize;
+
   while (l < leftSize && r < rightSize) {
     if (leftArray[l] < rightArray[r]) {
-      array[i] = leftArray[l];
+      arr[i] = leftArray[l];
+      i++;
       l++;
-      i++;
     } else {
-      array[i] = rightArray[r];
-      r++;
+      arr[i] = rightArray[r];
       i++;
+      r++;
     }
   }
-
   while (l < leftSize) {
-    array[i] = leftArray[l];
-    l++;
+    arr[i] = leftArray[l];
     i++;
+    l++;
   }
   while (r < rightSize) {
-    array[i] = rightArray[r];
-    r++;
+    arr[i] < rightArray[r];
     i++;
+    r++;
   }
+}
+
+void main(List<String> args) {
+  List arr = [7, 1, 3, 9, 8, 2, 4, 5];
+  print(arr);
+  mergeSort(arr);
+  print(arr);
 }
