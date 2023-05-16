8cbf34e3577ba886631f7ae7246f7b8c499ad2cd
MuhadJasiR <alijasir051@gmail.com>
2023-05-16 16:41:33 +0530

    review

--- workouts/sample_workouts.dart
+++ workouts/sample_workouts.dart
@@ -1,18 +1,5 @@
-insertionSort(List arr) {
-  for (var i = 0; i < arr.length - 1; i++) {
-    int temp = arr[i];
-    int j = i - 1;
-    while (j >= 0 && arr[j] > temp) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-
-    arr[j + 1] = temp;
+quickSort(List arr, int start, int end) {
+  if (end >= start) {
+    return;
   }
-}
-
-void main(List<String> args) {
-  List arr = [3, 1, 5, 2, 6];
-  insertionSort(arr);
-  print(arr);
 }
