560b0f34902f60aa89fcd478140ac82df0fc17cc
MuhadJasiR <alijasir051@gmail.com>
2023-05-24 11:26:31 +0530

    test commit

--- heap/heap.dart
+++ heap/heap.dart
@@ -1,174 +1,86 @@
-// class Heap {
-//   List heap = [];
-//   Heap(List array) {
-//     buildHeap(array);
-//   }
-//   void buildHeap(List array) {
-//     heap = array;
-//     for (int i = getParent(heap.length - 1); i >= 0; i--) {
-//       shiftDown(i);
-//     }
-//   }
-
-//   void shiftUp(int current) {
-//     int parentIdx = getParent(current);
-//     while (
-//         current >= 0 && heap.elementAt(parentIdx) > heap.elementAt(current)) {
-//       swap(current, parentIdx);
-//       current = parentIdx;
-//       parentIdx = getParent(current);
-//     }
-//   }
-
-//   void shiftDown(int current) {
-//     int lastIdx = heap.length - 1;
-//     int leftIdx = getLeftChild(current);
-//     while (leftIdx <= lastIdx) {
-//       int rightIdx = getRightChild(current);
-//       int shiftIdx = leftIdx;
-//       if (rightIdx <= lastIdx &&
-//           heap.elementAt(rightIdx) < heap.elementAt(leftIdx)) {
-//         shiftIdx = rightIdx;
-//       }
-//       if (heap.elementAt(shiftIdx) < heap.elementAt(current)) {
-//         swap(shiftIdx, current);
-//         current = shiftIdx;
-//         leftIdx = getLeftChild(current);
-//       }
-//     }
-//   }
-
-//   void peek() {
-//     print(heap[0]);
-//   }
-
-//   void remove() {
-//     swap(0, heap.length - 1);
-//     heap.removeLast();
-//     shiftDown(0);
-//   }
-
-//   void insert(int value) {
-//     heap.add(value);
-//     shiftUp(heap.length - 1);
-//   }
-
-//   int getParent(int i) {
-//     return (i - 1) ~/ 2;
-//   }
-
-//   int getLeftChild(int i) {
-//     return (i * 2) + 1;
-//   }
-
-//   int getRightChild(int i) {
-//     return (i * 2) + 2;
-//   }
-
-//   void display() {
-//     print(heap);
-//   }
-
-//   void swap(int i, int j) {
-//     int temp = heap[i];
-//     heap[i] = heap[j];
-//     heap[j] = temp;
-//   }
-// }
-
-// void main() {
-//   List<int> array = [4, 8, 9, 1, 3, 2, 8];
-//   Heap heap = Heap(array);
-//   heap.display();
-//   heap.peek();
-//   heap.remove();
-//   heap.display();
-// }
-
 class Heap {
   List heap = [];
-
-  Heap(this.heap);
-
-  buildHeap(List arr) {
-    heap = arr;
-    for (var i = getParentIdx(heap.length - 1); i > 0; i--) {
+  Heap(List array) {
+    buildHeap(array);
+  }
+  void buildHeap(List array) {
+    heap = array;
+    for (int i = getParent(heap.length - 1); i >= 0; i--) {
       shiftDown(i);
     }
   }
 
-  getParentIdx(int i) {
+  void shiftUp(int current) {
+    int parentIdx = getParent(current);
+    while (
+        current >= 0 && heap.elementAt(parentIdx) > heap.elementAt(current)) {
+      swap(current, parentIdx);
+      current = parentIdx;
+      parentIdx = getParent(current);
+    }
+  }
+
+  void shiftDown(int current) {
+    int lastIdx = heap.length - 1;
+    int leftIdx = getLeftChild(current);
+    while (leftIdx <= lastIdx) {
+      int rightIdx = getRightChild(current);
+      int shiftIdx = leftIdx;
+      if (rightIdx <= lastIdx &&
+          heap.elementAt(rightIdx) < heap.elementAt(leftIdx)) {
+        shiftIdx = rightIdx;
+      }
+      if (heap.elementAt(shiftIdx) < heap.elementAt(current)) {
+        swap(shiftIdx, current);
+        current = shiftIdx;
+        leftIdx = getLeftChild(current);
+      }
+    }
+  }
+
+  void peek() {
+    print(heap[0]);
+  }
+
+  void remove() {
+    swap(0, heap.length - 1);
+    heap.removeLast();
+    shiftDown(0);
+  }
+
+  void insert(int value) {
+    heap.add(value);
+    shiftUp(heap.length - 1);
+  }
+
+  int getParent(int i) {
     return (i - 1) ~/ 2;
   }
 
-  getLeftIdx(int i) {
+  int getLeftChild(int i) {
     return (i * 2) + 1;
   }
 
-  getRightIdx(int i) {
+  int getRightChild(int i) {
     return (i * 2) + 2;
-  }
-
-  swap(int i, int j) {
-    int temp = heap[i];
-    heap[i] = heap[j];
-    heap[j] = temp;
-  }
-
-  void shiftDown(int currentIdx) {
-    int endIdx = heap.length - 1;
-    int leftIdx = getLeftIdx(currentIdx);
-    while (leftIdx <= endIdx) {
-      int rightIdx = getRightIdx(currentIdx);
-      int idxToShift;
-      if (rightIdx <= endIdx &&
-          heap.elementAt(rightIdx) < heap.elementAt(leftIdx)) {
-        idxToShift = rightIdx;
-      } else {
-        idxToShift = leftIdx;
-      }
-      if (heap.elementAt(currentIdx) > heap.elementAt(idxToShift)) {
-        swap(currentIdx, idxToShift);
-        leftIdx = getLeftIdx(currentIdx);
-      } else {
-        return;
-      }
-    }
-  }
-
-  shiftUp(int currentIdx) {
-    int parent = getParentIdx(currentIdx);
-    while (
-        currentIdx > 0 && heap.elementAt(parent) > heap.elementAt(currentIdx)) {
-      swap(currentIdx, parent);
-      currentIdx = parent;
-      parent = getLeftIdx(currentIdx);
-    }
   }
 
   void display() {
     print(heap);
   }
 
-  insert(int value) {
-    heap.add(value);
-    shiftUp(heap.length - 1);
-  }
-
-  deletion() {
-    swap(heap.elementAt(0), heap.elementAt(heap.length - 1));
-    heap.removeLast();
-    shiftDown(0);
+  void swap(int i, int j) {
+    int temp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = temp;
   }
 }
 
-void main(List<String> args) {
-  List arr = [10, 40, 20, 50];
-  Heap heap = Heap(arr);
-  heap.insert(10);
-  heap.insert(20);
-  heap.insert(30);
-  heap.insert(40);
-  heap.insert(50);
+void main() {
+  List<int> array = [4, 8, 9, 1, 3, 2, 8];
+  Heap heap = Heap(array);
+  heap.display();
+  heap.peek();
+  heap.remove();
   heap.display();
 }
--- workouts/sample_workouts.dart
+++ workouts/sample_workouts.dart
@@ -1,1 +1,81 @@
+class Node {
+  int data;
+  Node? left;
+  Node? right;
+  Node(this.data);
+}
 
+class Bts {
+  Node? root;
+
+  insertion(int data) {
+    Node? current = root;
+    Node newNode = Node(data);
+    if (root == null) {
+      root = newNode;
+      return;
+    }
+    while (true) {
+      if (data < current!.data) {
+        if (current.left == null) {
+          current.left = newNode;
+          break;
+        } else {
+          current = current.left;
+        }
+      } else {
+        if (current.right == null) {
+          current.right = newNode;
+          break;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  contains(int data) {
+    Node? current = root;
+    while (current != null) {
+      if (data < current.data) {
+        current = current.left;
+      } else if (data > current.data) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(int data) {
+    removeHelper(data, root, null);
+  }
+
+  void removeHelper(int data, Node? currentNode, Node? parent) {
+    while (currentNode != null) {
+      if (data < currentNode.data) {
+        parent = currentNode;
+        currentNode = currentNode.left;
+      } else if (data > currentNode.data) {
+        parent = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        if (currentNode.left != null && currentNode.right != null) {
+          currentNode.data = minValue(currentNode.right);
+          removeHelper(currentNode.data, currentNode.right, currentNode);
+        } else {
+          if (parent == null) {}
+        }
+      }
+    }
+  }
+
+  int minValue(Node? currentNode) {
+    if (currentNode!.left == null) {
+      return currentNode.data;
+    } else {
+      return minValue(currentNode.left);
+    }
+  }
+}
